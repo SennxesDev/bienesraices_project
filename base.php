f68125340c0275665a2d81689db9504fa7d66702
Senn <100888352+SennxesDev@users.noreply.github.com>
2025-02-22 22:49:04 -0600

    Archivos PHP
    Cambios en la estructura del proyecto en donde los .html fueron reemplazados por .php, asi como la creación de la carpeta templates conteniendo los archivos footer.php y header.php usados de manera global en el proyecto

---
+++ base.php
@@ -0,0 +1,9 @@
+<?php include 'C:\xampp\htdocs\bienesraices_project\templates\header.php'; ?>
+
+    <main class="contenedor seccion">
+        <h1>Titulo Página</h1>
+    </main>
+
+<?php 
+    include 'C:\xampp\htdocs\bienesraices_project\templates\footer.php';   
+?>
